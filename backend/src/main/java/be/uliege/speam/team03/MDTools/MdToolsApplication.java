381439b14631cdd04efbc379b8641062fa9779e0
Irix0 <33395798+Irix0@users.noreply.github.com>
2024-11-03 12:42:16 +0100

    working svelete + springboot

---
+++ backend/src/main/java/be/uliege/speam/team03/MDTools/MdToolsApplication.java
@@ -0,0 +1,13 @@
+package be.uliege.speam.team03.MDTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdToolsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/be/uliege/speam/team03/MDTools/MdToolsApplicationTests.java
@@ -0,0 +1,13 @@
+// package be.uliege.speam.team03.MDTools;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class MdToolsApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
