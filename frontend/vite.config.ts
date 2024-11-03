a51169ad5350c6850f5a49f9b8941dd5b356dac2
Irix0 <33395798+Irix0@users.noreply.github.com>
2024-11-03 18:09:12 +0100

    fix permissions for svelte

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
     plugins: [sveltekit()],
 
+    server: {
+        port: 3000, // Set the port to 3000
+        strictPort: true // Fail if the port is already in use
+    },
+
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     }
