cc499b55e5ad9b88c7fb9bc58d063ec30dbf4347
Donato Sophia <sdonato@student.uliege.be>
2024-11-04 16:41:26 +0000

    mock search page

---
+++ frontend/src/tools.js
@@ -0,0 +1,7 @@
+export const tools = [
+    {id : 1, group :"paire de ciseaux", fct : "à dissection", name : "Kelly", form : "DR P/P A/REPLAT" , dim : "LG 160", src : "/cissors_1.png"},
+    {id : 2, group :"paire de ciseaux", fct : "à dissection", name : "Kelly", form : "DR P/P A/REPLAT" , dim : "LG 180", src : "/cissors_2.png"},
+    {id : 3, group :"paire de ciseaux", fct : "à dissection", name : "Kelly", form : "CB P/P A/REPLAT", dim : "LG 160", src : "/cissors_3.png"},
+    {id : 4, group :"paire de ciseaux", fct : "à dissection", name : "Kelly", form : "DR P/P TC A/REPLAT", dim : "LG 160", src : "/cissors_4.png"},
+    {id : 5, group :"paire de ciseaux", fct : "à dissection", name : "Kelly", form : "CB P/P TC A/REPLAT", dim :"LG 160", src : "/cissors_5.png"},
+];
