42958e8bdd418073afd017e34ef00c4b5a074f91
LouisBlaimont <louis.blaimont@gmail.com>
2024-11-11 15:11:18 +0100

    Script of db and sample_data example

---
+++ database/init.sql
@@ -0,0 +1,171 @@
+
+
+-- Table group
+CREATE TABLE "group" (
+    group_id SERIAL PRIMARY KEY,
+    group_name VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Table users
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role_name VARCHAR(50) NOT NULL, -- Note: 'admin or user'
+    job_position VARCHAR(100),
+    workplace VARCHAR(100)
+);
+
+-- Table logs
+CREATE TABLE logs (
+    log_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    action TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table supplier
+CREATE TABLE supplier (
+    supplier_id SERIAL PRIMARY KEY,
+    supplier_name VARCHAR(255) NOT NULL,
+    sold_by_md BOOLEAN NOT NULL,
+    closed BOOLEAN DEFAULT FALSE
+);
+
+-- Table sub_group
+CREATE TABLE sub_group (
+    sub_group_id SERIAL PRIMARY KEY,
+    group_id INTEGER REFERENCES "group"(group_id),
+    shape VARCHAR(255)
+);
+
+-- Table characteristic
+CREATE TABLE characteristic (
+    characteristic_id SERIAL PRIMARY KEY,
+    characteristic_name VARCHAR(255)
+);
+
+-- Table sub_group_characteristic
+CREATE TABLE sub_group_characteristic (
+    sub_group_id INTEGER REFERENCES sub_group(sub_group_id),
+    characteristic_id INTEGER REFERENCES characteristic(characteristic_id),
+    value VARCHAR(255),
+    value_abreviation VARCHAR(100),
+    PRIMARY KEY (sub_group_id, characteristic_id)
+);
+
+-- Table instruments
+CREATE TABLE instruments (
+    instrument_id SERIAL PRIMARY KEY,
+    supplier_id INTEGER REFERENCES supplier(supplier_id),
+    sub_group_id INTEGER REFERENCES sub_group(sub_group_id),
+    reference VARCHAR(255) NOT NULL,
+    supplier_description TEXT,
+    price NUMERIC(10, 2) NOT NULL,
+    obsolete BOOLEAN DEFAULT FALSE
+);
+
+-- Table group_characteristic
+CREATE TABLE group_characteristic (
+    group_id INTEGER REFERENCES "group"(group_id),
+    characteristic_id INTEGER REFERENCES characteristic(characteristic_id),
+    order_position INTEGER,
+    PRIMARY KEY (group_id, characteristic_id)
+);
+
+-- Table alternatives
+CREATE TABLE alternatives (
+    instruments_id_1 INTEGER REFERENCES instruments(instrument_id),
+    instruments_id_2 INTEGER REFERENCES instruments(instrument_id),
+    PRIMARY KEY (instruments_id_1, instruments_id_2)
+);
+
+-- Table orders
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table order_items
+CREATE TABLE order_items (
+    order_id INTEGER REFERENCES orders(order_id),
+    instrument_id INTEGER REFERENCES instruments(instrument_id),
+    quantity INTEGER NOT NULL,
+    PRIMARY KEY (order_id, instrument_id)
+);
+-- FUNCTION: public.check_alternative_constraints()
+
+-- DROP FUNCTION IF EXISTS public.check_alternative_constraints();
+
+CREATE OR REPLACE FUNCTION public.check_alternative_constraints()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+    -- Ensure instruments are from different suppliers
+    IF (SELECT supplier_id FROM instruments WHERE instrument_id = NEW.instruments_id_1) = 
+       (SELECT supplier_id FROM instruments WHERE instrument_id = NEW.instruments_id_2) THEN
+        RAISE EXCEPTION 'Instruments must be from different suppliers';
+    END IF;
+
+    -- Ensure instruments are from the same group by checking through sub_group and group
+    IF (SELECT group_id FROM sub_group 
+        WHERE sub_group_id = (SELECT sub_group_id FROM instruments WHERE instrument_id = NEW.instruments_id_1)) <> 
+       (SELECT group_id FROM sub_group 
+        WHERE sub_group_id = (SELECT sub_group_id FROM instruments WHERE instrument_id = NEW.instruments_id_2)) THEN
+        RAISE EXCEPTION 'Instruments must be from the same group';
+    END IF;
+
+    RETURN NEW;
+END;
+$BODY$;
+
+ALTER FUNCTION public.check_alternative_constraints()
+    OWNER TO postgres;
+
+CREATE TRIGGER verify_alternative_constraints
+BEFORE INSERT OR UPDATE ON alternatives
+FOR EACH ROW
+EXECUTE FUNCTION check_alternative_constraints();
+CREATE OR REPLACE FUNCTION update_shape()
+RETURNS TRIGGER AS $$
+DECLARE
+    characteristic_record RECORD;
+    shape_text TEXT := '';
+BEGIN
+    -- Construire la chaîne shape en fonction des caractéristiques présentes et de leur ordre
+    FOR characteristic_record IN
+        SELECT sgc.value_abreviation
+        FROM sub_group_characteristic sgc
+        JOIN group_characteristic gc ON gc.group_id = (SELECT group_id FROM sub_group WHERE sub_group_id = NEW.sub_group_id)
+                                     AND gc.characteristic_id = sgc.characteristic_id
+        WHERE sgc.sub_group_id = NEW.sub_group_id
+          AND gc.order_position IS NOT NULL
+        ORDER BY gc.order_position
+    LOOP
+        -- Concaténer chaque value_abreviation avec un '/' comme séparateur
+        shape_text := shape_text || characteristic_record.value_abreviation || '/';
+    END LOOP;
+
+    -- Supprimer le dernier séparateur '/' à la fin de la chaîne
+    shape_text := RTRIM(shape_text, '/');
+
+    -- Mettre à jour le champ shape dans sub_group
+    UPDATE sub_group
+    SET shape = shape_text
+    WHERE sub_group_id = NEW.sub_group_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_shape
+AFTER INSERT OR UPDATE ON sub_group_characteristic
+FOR EACH ROW
+EXECUTE FUNCTION update_shape();
---
+++ database/sample_data.sql
@@ -0,0 +1,78 @@
+
+/* if problem with sequence you can add this:
+ALTER SEQUENCE "group_group_id_seq" RESTART WITH 1;
+ALTER SEQUENCE users_user_id_seq RESTART WITH 1;
+ALTER SEQUENCE sub_group_sub_group_id_seq RESTART WITH 1;
+ALTER SEQUENCE characteristic_characteristic_id_seq RESTART WITH 1;
+ALTER SEQUENCE supplier_supplier_id_seq RESTART WITH 1;
+ALTER SEQUENCE instruments_instrument_id_seq RESTART WITH 1;
+*/
+
+
+-- Insert into "group" table
+INSERT INTO "group" (group_name) VALUES 
+('Scalpels'), 
+('Scissors'), 
+('Forceps');
+
+-- Insert into users table
+INSERT INTO users (username, email, password, role_name, job_position, workplace) VALUES 
+('john_doe', 'john@example.com', 'hashed_password_123', 'user', 'Surgeon', 'City Hospital'),
+('jane_admin', 'jane@example.com', 'hashed_password_456', 'admin', 'IT Manager', 'Head Office');
+
+-- Insert into logs table
+INSERT INTO logs (user_id, action) VALUES 
+(1, 'Logged in'),
+(2, 'Changed settings'),
+(1, 'Added new instrument');
+
+-- Insert into supplier table
+INSERT INTO supplier (supplier_name, sold_by_md, closed) VALUES 
+('Medicon', TRUE, FALSE),
+('Geomed', TRUE, FALSE);
+
+-- Insert into sub_group table
+INSERT INTO sub_group (group_id, shape) VALUES 
+(1, NULL), 
+(1, NULL), 
+(3, NULL);
+
+-- Insert into characteristic table
+INSERT INTO characteristic (characteristic_name) VALUES 
+('Length'), 
+('Material'), 
+('Sharpness');
+
+-- Insert into sub_group_characteristic table
+INSERT INTO sub_group_characteristic (sub_group_id, characteristic_id, value, value_abreviation) 
+VALUES
+(1, 1, 'Large', 'L'),      -- volume, avec "L" comme abrégé
+(1, 2, 'Curved', 'C')      -- curve, avec "C" comme abrégé
+(2, 1, '10cm', '10CM');
+
+-- Insert into instruments table
+INSERT INTO instruments (supplier_id, sub_group_id, reference, supplier_description, price, obsolete) VALUES 
+(1, 1, 'SC-1234', '15cm Scalpel', 15.99, FALSE),
+(2, 2, 'SC-5678', '15cm Scalpel de geomed', 10.50, FALSE),
+(2, 3, 'FR-0001', 'Forceps with lock', 20.75, TRUE);
+
+-- Insert into group_characteristic table
+INSERT INTO group_characteristic (group_id, characteristic_id, order_position) VALUES 
+(1, 1, 1), 
+(1, 2, 2), 
+(2, 1, 1);
+
+-- Insert into alternatives table
+INSERT INTO alternatives (instruments_id_1, instruments_id_2) VALUES 
+(1, 2);
+
+-- Insert into orders table
+INSERT INTO orders (user_id) VALUES 
+(1), 
+(2);
+
+-- Insert into order_items table
+INSERT INTO order_items (order_id, instrument_id, quantity) VALUES 
+(1, 1, 5), 
+(2, 2, 3), 
+(2, 3, 2);
