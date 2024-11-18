996ea26c9132a2e460de13f68db5adc65acef9dc
Louan Robert <1413-louan.robert@users.noreply.gitlab.uliege.be>
2024-11-18 15:10:43 +0100

    add backend test to pipeline

---
+++ backend/src/test/java/be/uliege/speam/team03/MDTools/EmptyTest.java
@@ -0,0 +1,13 @@
+package be.uliege.speam.team03.MDTools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EmptyTest {
+
+    @Test
+    void testPipeline() {
+        assertTrue(true, "This test is just to check the pipeline setup.");
+    }
+}
