49b29e4a4e4d15ea519dec0b23981582c4a03b92
Louan Robert <1413-louan.robert@users.noreply.gitlab.uliege.be>
2024-11-20 15:32:03 +0100

    improve webmaster view

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,26 +1,46 @@
 <script>
-	import "../app.css";
-	let { children } = $props();
+  import "../app.css";
+  import { SvelteToast } from "@zerodevx/svelte-toast";
+
+  let { children } = $props();
 </script>
 
-
-
 <header class="bg-teal-500 h-16 flex items-center justify-between px-6">
-	<!-- Logo -->
-	<img alt="Logo MD" src="logo-blanc.png" class="h-full" />
- 
-	<!-- Navigation Bar -->
-	<nav class="space-x-6 hidden md:flex"> <!-- Hide nav items on small screens (md and below) -->
-	  <a href="/" class="text-white hover:text-teal-200 transition duration-300">Home Client</a>
-	  <a href="/home_2" class="text-white hover:text-teal-200 transition duration-300">Home Admin</a>
-	  <a href="/add_group" class="text-white hover:text-teal-200 transition duration-300">Add Group</a>
-	  <a href="/login" class="text-white hover:text-teal-200 transition duration-300">Login</a>
-	  <a href="/psw_forgot" class="text-white hover:text-teal-200 transition duration-300">Password Forgot</a>
-	  <a href="/sign_up" class="text-white hover:text-teal-200 transition duration-300">Sign Up</a>
-	  <a href="/searches" class="text-white hover:text-teal-200 transition duration-300">Searches Client</a>
-	  <a href="/searches_admin" class="text-white hover:text-teal-200 transition duration-300">Searches Admin</a>
-	  <a href="/webmaster" class="text-white hover:text-teal-200 transition duration-300">Webmaster</a>
-	</nav>
+  <!-- Logo -->
+  <img alt="Logo MD" src="logo-blanc.png" class="h-full" />
+
+  <!-- Navigation Bar -->
+  <nav class="space-x-6 hidden md:flex">
+    <!-- Hide nav items on small screens (md and below) -->
+    <a href="/" class="text-white hover:text-teal-200 transition duration-300">Home Client</a>
+    <a href="/home_2" class="text-white hover:text-teal-200 transition duration-300">Home Admin</a>
+    <a href="/add_group" class="text-white hover:text-teal-200 transition duration-300">Add Group</a
+    >
+    <a href="/login" class="text-white hover:text-teal-200 transition duration-300">Login</a>
+    <a href="/psw_forgot" class="text-white hover:text-teal-200 transition duration-300"
+      >Password Forgot</a
+    >
+    <a href="/sign_up" class="text-white hover:text-teal-200 transition duration-300">Sign Up</a>
+    <a href="/searches" class="text-white hover:text-teal-200 transition duration-300"
+      >Searches Client</a
+    >
+    <a href="/searches_admin" class="text-white hover:text-teal-200 transition duration-300"
+      >Searches Admin</a
+    >
+    <a href="/webmaster" class="text-white hover:text-teal-200 transition duration-300">Webmaster</a
+    >
+  </nav>
 </header>
 
 {@render children()}
+
+<SvelteToast />
+
+<style>
+  :root {
+    --toastContainerTop: 4.25rem;
+    --toastContainerRight: 1rem;
+    --toastContainerBottom: auto;
+    --toastContainerLeft: auto;
+  }
+</style>
--- frontend/src/routes/webmaster/+page.svelte
+++ frontend/src/routes/webmaster/+page.svelte
@@ -1,27 +1,28 @@
 <script>
-  import { goto } from '$app/navigation'; // Import SvelteKit navigation helper
+  import { goto } from "$app/navigation"; // Import SvelteKit navigation helper
+  import { toast } from "@zerodevx/svelte-toast";
 
   let users = [
-    { id: "1", email: "user1@example.com" },
-    { id: "2", email: "user2@example.com" },
-    { id: "3", email: "user3@example.com" },
-    { id: "4", email: "admin@example.com" }
+    { id: "1", email: "user1@example.com", roles: ["admin"], enabled: false },
+    { id: "2", email: "user2@example.com", roles: ["user"], enabled: true },
+    { id: "3", email: "user3@example.com", roles: ["admin", "webmaster"], enabled: true },
+    { id: "4", email: "admin@example.com", roles: ["user"], enabled: true },
   ]; // This would normally come from an API
 
   let email = "";
-  let searchQuery = "";  // New search query state
+  let searchQuery = ""; // New search query state
   let loading = false;
 
   // Action functions
   const createUser = async () => {
     if (!email) {
-      alert('Please enter an email address.');
+      toast.push("Please enter a valid email address");
       return;
     }
     loading = true;
     // Simulate API call with a delay
     setTimeout(() => {
-      alert(`User with email ${email} created!`);
+      toast.push(`User with email ${email} created!`);
       users.push({ id: (users.length + 1).toString(), email });
       email = "";
       loading = false;
@@ -29,36 +30,80 @@
   };
 
   const disableAccount = (userId) => {
-    if (confirm(`Are you sure you want to disable the account with ID ${userId}?`)) {
-      alert(`Account with ID ${userId} disabled!`);
+    const userIndex = users.findIndex((u) => u.id === userId);
+    if (userIndex !== -1) {
+      users[userIndex].enabled = !users[userIndex].enabled; // Toggle status
+      toast.push(
+        users[userIndex].enabled
+          ? `Account with ID ${userId} enabled!`
+          : `Account with ID ${userId} disabled!`
+      );
     }
   };
 
   const resetPassword = (userId) => {
     if (confirm(`Are you sure you want to reset the password for user ID ${userId}?`)) {
-      alert(`Password reset for user ID ${userId}`);
+      toast.push(`Password reset for user ID ${userId}`);
     }
   };
 
   // Navigate to the logs page
   const viewLogs = () => {
-    goto('/webmaster/logs');
+    goto("/webmaster/logs");
+  };
+
+  const getBadges = (userId) => {
+    const user = users.find((u) => u.id === userId);
+    if (user) {
+      return user.roles.map((role) => {
+        if (role === "admin") {
+          return {
+            text: "Administrateur",
+            bg: "bg-red-100",
+            textColor: "text-red-800",
+            ringColor: "ring-red-800/10",
+          };
+        } else if (role === "webmaster") {
+          return {
+            text: "Webmaster",
+            bg: "bg-green-100",
+            textColor: "text-green-800",
+            ringColor: "ring-green-800/10",
+          };
+        } else if (role === "user") {
+          return {
+            text: "Utilisateur",
+            bg: "bg-blue-100",
+            textColor: "text-blue-800",
+            ringColor: "ring-blue-800/10",
+          };
+        } else {
+          return {
+            text: role,
+            bg: "bg-gray-100",
+            textColor: "text-gray-800",
+            ringColor: "ring-gray-800/10",
+          };
+        }
+      });
+    }
+    return [];
   };
 
   // Filter users based on the search query
-  $: filteredUsers = users.filter(user => user.email.toLowerCase().includes(searchQuery.toLowerCase()));
+  $: filteredUsers = users.filter((user) =>
+    user.email.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 </script>
 
 <div class="p-8 space-y-10">
-  <!-- User Management Section -->
   <section class="bg-white shadow-lg rounded-xl p-6">
-    <h2 class="text-xl font-semibold mb-8">Website Administration</h2>
+    <h2 class="text-2xl font-semibold mb-8">Website Administration</h2>
 
-    <!-- Create User Form and Consult Logs Button in the same row -->
-    <div class="flex items-start space-x-6 mb-8">
-      <div class="flex-1 max-w-xs bg-gray-50 border border-gray-300 rounded-lg p-6">
-        <h3 class="text-lg font-semibold mb-4">Create User</h3>
-        <div class="space-y-6">
+    <div class="flex flex-col lg:flex-row items-start lg:space-x-6 space-y-6 lg:space-y-0 mb-8">
+      <div class="flex-1 max-w-md bg-gray-50 border border-gray-300 rounded-lg p-6">
+        <h3 class="text-lg font-medium mb-4">Create User</h3>
+        <div class="space-y-4">
           <input
             type="email"
             bind:value={email}
@@ -67,73 +112,136 @@
           />
           <button
             on:click={createUser}
-            class="w-full bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition disabled:opacity-50"
+            class="w-full bg-gradient-to-r from-green-500 to-green-700 text-white px-6 py-3 rounded-lg hover:scale-105 transform transition disabled:opacity-50 flex items-center justify-center space-x-2"
             disabled={loading}
           >
             {#if loading}
-              Creating...
+              <span class="animate-spin h-5 w-5 border-t-2 border-white rounded-full"></span>
+              <span>Creating...</span>
             {:else}
-              Create User
+              <span>Create User</span>
             {/if}
           </button>
         </div>
       </div>
 
-      <!-- Consult Logs Button -->
-      <div class="flex-1 max-w-xs bg-gray-50 border border-gray-300 rounded-lg p-6">
-        <h3 class="text-lg font-semibold mb-4">Consult Logs</h3> <!-- Empty header for alignment -->
+      <div class="flex-1 max-w-md bg-gray-50 border border-gray-300 rounded-lg p-6">
+        <h3 class="text-lg font-medium mb-4">Consult Logs</h3>
         <button
           on:click={viewLogs}
-          class="w-full bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition ease-in-out duration-200 transform hover:scale-105"
+          class="w-full bg-gradient-to-r from-blue-500 to-blue-700 text-white px-6 py-3 rounded-lg hover:scale-105 transform transition"
         >
           View Logs
         </button>
       </div>
     </div>
 
-    <!-- Users Table -->
-    <h3 class="text-lg font-semibold mb-6">Existing Users</h3>
-    
-    <!-- Search Field -->
-    <div class="mb-6 max-w-xs">
-      <input
-        type="text"
-        bind:value={searchQuery}
-        placeholder="Search by email"
-        class="w-full p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-      />
-    </div>
+    <!-- Users List Section -->
+    <div class="bg-gray-50 border border-gray-300 rounded-lg p-6">
+      <h3 class="text-lg font-medium mb-6">Existing Users</h3>
 
-    <div class="overflow-x-auto">
-      <table class="min-w-full table-auto border-collapse">
-        <thead>
-          <tr class="bg-gray-100 text-left text-sm font-semibold">
-            <th class="px-6 py-4">User Email</th>
-            <th class="px-6 py-4">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each filteredUsers as user}
-            <tr class="border-b hover:bg-gray-50">
-              <td class="px-6 py-4">{user.email}</td>
-              <td class="px-6 py-4 space-x-3">
-                <button
-                  on:click={() => disableAccount(user.id)}
-                  class="bg-red-600 text-white px-5 py-2 rounded-lg hover:bg-red-700 transition ease-in-out duration-200 transform hover:scale-105"
-                >
-                  Disable
-                </button>
-                <button
-                  on:click={() => resetPassword(user.id)}
-                  class="bg-yellow-500 text-white px-5 py-2 rounded-lg hover:bg-yellow-600 transition ease-in-out duration-200 transform hover:scale-105"
-                >
-                  Reset Password
-                </button>
-              </td>
+      <!-- Search Bar -->
+      <div class="mb-6 max-w-xs">
+        <input
+          type="text"
+          bind:value={searchQuery}
+          placeholder="Search by email"
+          class="w-full p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
+
+      <!-- Users Table -->
+      <div class="overflow-x-auto">
+        <table class="min-w-full table-auto border-collapse">
+          <thead>
+            <tr class="bg-gray-100 text-left text-sm font-semibold">
+              <th class="px-6 py-4">User Email</th>
+              <th class="px-6 py-4">Roles</th>
+              <th class="px-6 py-4">Actions</th>
             </tr>
-          {/each}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {#each filteredUsers as user}
+              <tr class="border-b hover:bg-gray-50 transition">
+                <td class="px-6 py-4">
+                  {user.email}
+                  {#if user.enabled}
+                    <span
+                      class="rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-700/10"
+                      >Actif</span
+                    >
+                  {:else}
+                    <span
+                      class="rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10"
+                      >Désactivé</span
+                    >
+                  {/if}
+                </td>
+                <td class="px-6 py-4 space-x-1">
+                  {#each getBadges(user.id) as badge}
+                    <span
+                      class={`px-2 py-1 rounded-md text-xs font-medium ring-1 ring-inset ${badge.bg} ${badge.textColor} ${badge.ringColor}`}
+                    >
+                      {badge.text}
+                    </span>
+                  {/each}
+                </td>
+                <td class="px-6 py-4 inline-flex rounded-md shadow-sm">
+                  <button
+                    on:click={() => disableAccount(user.id)}
+                    class={`px-5 py-2 rounded-l-lg w-32 flex items-center transform transition ${
+                      user.enabled
+                        ? "bg-red-600 hover:bg-red-700 text-white"
+                        : "bg-green-600 hover:bg-green-700 text-white"
+                    }`}
+                  >
+                    <span
+                      ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                        />
+                      </svg>
+                    </span>
+                    <span class="pl-2">{user.enabled ? "Disable" : "Enable"}</span>
+                  </button>
+
+                  <button
+                    on:click={() => resetPassword(user.id)}
+                    class="bg-yellow-500 text-white px-5 py-2 rounded-r-lg hover:bg-yellow-600 flex items-center transform transition"
+                  >
+                    <span
+                      ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                        />
+                      </svg>
+                    </span>
+                    <span class="pl-2">Reset Password</span>
+                  </button>
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
     </div>
   </section>
 </div>
