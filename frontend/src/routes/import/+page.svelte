3238f07790c94940a1b7c9a04dc7c746b074c3a8
LouisBlaimont <louis.blaimont@gmail.com>
2024-11-27 11:36:48 +0100

    modif modal import

--- frontend/src/routes/import/+page.svelte
+++ frontend/src/routes/import/+page.svelte
@@ -9,12 +9,15 @@
     let offset = { x: 0, y: 0 };
     let isDraggingModal = false;
     let currentView = "main";
-    let selectedGroup = "Classique";
+    let selectedGroup = "";
     let selectedSubGroup = "";
     let isNextEnabled = false;
     let selectedOption = "";
-    const groups = ["Pinces", "Ciseaux", "Autre..."];
-    let subGroups = { "Classique": ["Sous-groupe 1", "Sous-groupe 2"], "Pinces": ["Sous-groupe A", "Sous-groupe B"], "Ciseaux": ["Sous-groupe X", "Sous-groupe Y"], "Autre": [] };
+    let missingColumns = [];
+    let presentColumns = [];
+    const groups = ["Pinces", "Ciseaux"];
+    let subGroups = { "Classique": ["Sous-groupe 1", "Sous-groupe 2"], "Pinces": ["Sous-groupe A", "Sous-groupe B"], "Ciseaux": ["Sous-groupe X", "Sous-groupe Y"]};
+    const requiredColumns = ["Nom", "Description", "Quantité", "Prix"];
   
     const handleDrop = (event) => {
       event.preventDefault();
@@ -112,21 +115,33 @@
         currentView = "sous-groupe";
         isNextEnabled = false;
         selectedSubGroup = "";
+      } else if (currentView === "sous-groupe" && isNextEnabled) {
+        currentView = "verification";
+        verifyColumns();
       }
     };
   
     const handleGroupChange = (event) => {
       selectedGroup = event.target.value;
-      if (selectedGroup === "Autre") {
-        selectedGroup = "";
-        subGroups["Autre"] = [];
-      }
-      selectedSubGroup = "Classique";
-      isNextEnabled = true;
+      selectedSubGroup = "";
+      isNextEnabled = false;
     };
   
     const handleSubGroupChange = (event) => {
       selectedSubGroup = event.target.value;
+      isNextEnabled = selectedGroup !== "" && selectedSubGroup !== "";
+    };
+  
+    const verifyColumns = () => {
+      // Example logic for verifying columns - placeholder
+      const uploadedColumns = ["Nom", "Quantité", "Prix"];
+      presentColumns = uploadedColumns.filter(col => requiredColumns.includes(col));
+      missingColumns = requiredColumns.filter(col => !uploadedColumns.includes(col));
+    };
+  
+    const handleImportFinal = () => {
+      console.log("Importation terminée avec les colonnes correctes");
+      showModal = false;
     };
   
     onMount(() => {
@@ -136,9 +151,6 @@
   </script>
   
   <main class="w-full flex flex-col items-center">
-    <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <!-- svelte-ignore a11y-mouse-events-have-key-events -->
     <div class="w-3/4 h-64 border-4 border-dashed border-gray-500 rounded-lg flex items-center justify-center mt-6 bg-gray-100" role="button" tabindex="0"
       on:drop={handleDrop}
       on:dragover={handleDragOver}
@@ -165,7 +177,8 @@
     {/if}
   
     <form class="w-3/4 mt-4" on:submit={handleImport}>
-      <input type="file" accept=".xlsx, .xls" class="hidden" bind:this={fileInput} on:change={handleFileSelection} />
+      <label for="file-upload" class="block mb-2 text-gray-700">Choisir un fichier :</label>
+      <input id="file-upload" type="file" accept=".xlsx, .xls" class="hidden" bind:this={fileInput} on:change={handleFileSelection} />
       <button class="bg-gray-700 text-white py-2 px-4 rounded-lg mt-2" type="button" on:click={() => fileInput.click()}>Choisir un fichier</button>
       {#if file}
         <button class="bg-blue-600 text-white py-2 px-4 rounded-lg mt-2 ml-2" type="submit">Importer</button>
@@ -206,38 +219,49 @@
               <h2 class="text-2xl font-bold">Sélectionnez le groupe et sous-groupe</h2>
             </div>
             <div class="mb-6">
-              <!-- svelte-ignore a11y_label_has_associated_control -->
-              <label class="block mb-2 text-gray-700">Groupe :</label>
-              <select bind:value={selectedGroup} class="w-full p-3 border rounded" on:change={handleGroupChange}>
-                {#each groups as group}
+              <label for="group-select" class="block mb-2 text-gray-700">Groupe :</label>
+              <input id="group-select" type="text" bind:value={selectedGroup} class="w-full p-3 border rounded" placeholder="Entrez un groupe" on:input={handleGroupChange} list="group-options" />
+              <datalist id="group-options">
+                {#each groups.filter(group => group.toLowerCase().includes(selectedGroup.toLowerCase())) as group}
                   <option value={group}>{group}</option>
                 {/each}
-                <option value="Autre">Autre...</option>
-              </select>
-              {#if selectedGroup === ""}
-                <input type="text" class="w-full p-3 border rounded mt-2" placeholder="Entrez un nouveau groupe" bind:value={selectedGroup} />
-              {/if}
+              </datalist>
             </div>
             <div class="mb-6">
-              <label class="block mb-2 text-gray-700">Sous-groupe :</label>
-              <select bind:value={selectedSubGroup} class="w-full p-3 border rounded" on:change={handleSubGroupChange}>
-                {#each (subGroups[selectedGroup] || []) as subGroup}
+              <label for="subgroup-select" class="block mb-2 text-gray-700">Sous-groupe :</label>
+              <input id="subgroup-select" type="text" bind:value={selectedSubGroup} class="w-full p-3 border rounded" placeholder="Entrez un sous-groupe" on:input={handleSubGroupChange} list="subgroup-options" disabled={selectedGroup === ""} />
+              <datalist id="subgroup-options">
+                {#each (subGroups[selectedGroup] || []).filter(subGroup => subGroup.toLowerCase().includes(selectedSubGroup.toLowerCase())) as subGroup}
                   <option value={subGroup}>{subGroup}</option>
                 {/each}
-                <option value="Autre">Autre...</option>
-              </select>
-              {#if selectedSubGroup === "Autre"}
-                <input type="text" class="w-full p-3 border rounded mt-2" placeholder="Entrez un nouveau sous-groupe" bind:value={selectedSubGroup} />
-              {/if}
+              </datalist>
             </div>
+          {:else if currentView === "verification"}
+            <h2 class="text-2xl font-bold mb-6">Vérification des Colonnes</h2>
+            <p class="text-gray-700 mb-4">Voici les colonnes présentes et manquantes dans le fichier Excel :</p>
+            <div class="mb-6">
+              <h3 class="text-lg font-bold text-green-700 mb-2">Colonnes Présentes :</h3>
+              <ul class="list-disc pl-6">
+                {#each presentColumns as column}
+                  <li class="text-gray-800">{column}</li>
+                {/each}
+              </ul>
+            </div>
+            <div class="mb-6">
+              <h3 class="text-lg font-bold text-red-700 mb-2">Colonnes Manquantes :</h3>
+              <ul class="list-disc pl-6">
+                {#each missingColumns as column}
+                  <li class="text-gray-800">{column}</li>
+                {/each}
+              </ul>
+            </div>
+            <button 
+              class="border border-gray-500 text-gray-500 py-2 px-4 rounded-lg absolute bottom-0 right-0 mb-2 mr-2 bg-blue-600 text-white"
+              on:click={handleImportFinal}
+            >
+              Importer
+            </button>
           {/if}
-          <button 
-            class="border border-gray-500 text-gray-500 py-2 px-4 rounded-lg absolute bottom-0 right-0 mb-2 mr-2"
-            style="opacity: {isNextEnabled ? 1 : 0.5}; pointer-events: {isNextEnabled ? 'auto' : 'none'}; background-color: {isNextEnabled ? '#c3dafe' : 'transparent'}; color: {isNextEnabled ? '#2d3748' : '#gray-500'};"
-            on:click={handleNext}
-          >
-            Suivant
-          </button>
         </div>
       </div>
     {/if}
@@ -259,14 +283,4 @@
     .shadow-xl {
       box-shadow: 0 20px 25px rgba(0, 0, 0, 0.15);
     }
-    .button-next {
-      border: 1px solid #ccc;
-      color: #888;
-      transition: all 0.3s ease;
-    }
-    .button-next.enabled {
-      border-color: #444;
-      color: #444;
-    }
   </style>
-  
