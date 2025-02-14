f85ad9cc1101bb05f58f0043e6625b6c25e4098c
Macha.Krumm <macha.krumm@student.uliege.be>
2025-02-14 08:30:59 +0100

    rbac navigation implementation

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,25 @@
 <script>
   import "../app.css";
   import { SvelteToast } from "@zerodevx/svelte-toast";
+  import Loading from "$lib/Loading.svelte";
+	import { checkRole } from "$lib/rbacUtils";
+	import { ROLES } from "../constants";
+	import { user } from "../stores"; 
 
   let { children } = $props();
 
-  // Simulating user roles (Replace with actual authentication)
-  let userRole = "user"; // Change to "admin" for admin view
+  // simulating user roles
+  let userRole = "simpleUser"; // change to "admin" for admin view
+  let userValue;
+
+  // Subscribe to store
+  user.subscribe(value => {
+    userValue = value;
+  });
+
+  // Use reactive declarations
+  let isAdmin = checkRole(userValue, ROLES.ADMIN);
+  let isCustomer = checkRole(userValue, ROLES.CUSTOMER);
 </script>
 
 <header class="bg-teal-500 h-16 flex items-center justify-between px-6">
@@ -16,7 +30,11 @@
 
   <!-- Navigation Bar -->
   <nav class="space-x-6 hidden md:flex">
-    {#if userRole === "user"}
+    <a href="/login" class="text-white hover:text-teal-200 transition duration-300">
+      Login
+    </a>
+    <!--{#if isCustomer}-->
+    {#if userRole == "simpleUser"}
       <a href="/" class="text-white hover:text-teal-200 transition duration-300">
         Home Client
       </a>
@@ -34,7 +52,8 @@
       </a>
     {/if}
 
-    {#if userRole === "admin"}
+    <!--{#if isAdmin}-->
+    {#if userRole == "admin"}
       <a href="/home_2" class="text-white hover:text-teal-200 transition duration-300">
         Home Admin
       </a>
@@ -56,7 +75,7 @@
     {/if}
   </nav>
 
-  <a class="mr-6" href="/">
+  <a class="mr-6" href="/login">
     <button class="text-black bg-gray-300 rounded hover:bg-gray-700" type="submit">Log out</button>
   </a>
 </header>
