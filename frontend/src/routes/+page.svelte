ce9957d8b6649c0a04f80f87ba31be5d9fed37e2
sophiadonato <sdonato@proximus.be>
2025-02-15 10:50:55 +0100

    see correct group names

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,30 @@
-<script lang="ts">
-  /*const images = import.meta.glob('/Groups_img/*.png', { as: 'url' });
-  console.log(images);
-
-  let imageUrls: string[] = [];
-  Promise.all(
-    Object.values(images).map(loader => loader())).then(urls => {imageUrls = urls;}
-  );
-  console.log(images);*/
+<script >
+  import { onMount } from 'svelte';
+  // const images: Record<string, {default: string}> = import.meta.glob('/groups/*.png', {eager: true});
   const imageUrls = [
     '/Groups_img/1.png','/Groups_img/2.png','/Groups_img/3.png','/Groups_img/4.png','/Groups_img/5.png','/Groups_img/6.png','/Groups_img/7.png',
     '/Groups_img/8.png','/Groups_img/9.png','/Groups_img/10.png','/Groups_img/11.png','/Groups_img/12.png','/Groups_img/13.png','/Groups_img/14.png',
   ];
+
+  let groups_summary = [];
+  let groups = [];
+  let number_of_tool = [];
+  onMount(async () => {
+            try {
+                const response = await fetch('http://localhost:8080/api/groups/summary');
+
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch groups: ${response.statusText}`);
+                }
+
+                groups_summary = await response.json();
+            } catch (error) {
+                console.error(error);
+                errorMessage = error.message;
+            }
+        });
+    $: groups = groups_summary.map(group => group.name);
+    $: number_of_tool = groups_summary.map(group => group.nbInstr);
 </script>
 
 <head><title>Accueil</title></head>
@@ -48,11 +62,11 @@
   </aside>
 
   <section class="w-full md:w-3/4 grid grid-cols-2 sm:grid-cols-3 sm:min-w-[600px] lg:grid-cols-4 lg:min-w-[900px] xl:min-w-[1200px] gap-6 p-4 border border-gray-300 rounded-lg shadow-md max-h-[500px] overflow-y-auto">
-    {#each imageUrls as imageUrl}
+    {#each groups as group, index}
     <div class="relative group">
-      <img class="w-full object-cover rounded-lg" src={imageUrl} alt="group_img"/>
+      <img class="w-full object-cover rounded-lg" src='/Groups_img/1.png' alt="group_img"/>
       <div class="absolute bottom-0 left-0 p-2 text-white text-xs">
-        Your overlay text here
+        {group} ({number_of_tool[index]})
       </div>
     </div>
     {/each}
