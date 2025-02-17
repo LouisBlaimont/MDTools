f620efb9a73a247ce1b76e69a36146a8f53da98f
Macha.Krumm <macha.krumm@student.uliege.be>
2025-02-17 15:51:49 +0100

    changed categoryController

--- backend/src/main/java/be/uliege/speam/team03/MDTools/controllers/InstrumentController.java
+++ backend/src/main/java/be/uliege/speam/team03/MDTools/controllers/InstrumentController.java
@@ -1,17 +1,11 @@
 package be.uliege.speam.team03.MDTools.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 import be.uliege.speam.team03.MDTools.models.Instruments;
 import be.uliege.speam.team03.MDTools.repositories.InstrumentRepository;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import be.uliege.speam.team03.MDTools.DTOs.InstrumentDTO;
-import be.uliege.speam.team03.MDTools.services.InstrumentService;
-
 
 @RestController
 public class InstrumentController {
@@ -23,17 +17,5 @@ public class InstrumentController {
     @GetMapping("/instruments")
     public Iterable<Instruments> findallInstruments(){
         return this.instrumentRepository.findAll();
-    }
-
-     // retrieve all instruments of a certain category 
-    @GetMapping("/{categoryId}")
-    public ResponseEntity<?> getInstrumentsOfCategory(@PathVariable Integer categoryId) {
-        // calls the service corresponding with the correct function 
-        List<InstrumentDTO> instruments = instrumentService.findInstrumentsOfCatergory(categoryId);
-        // checking if we found something 
-        if (instruments == null || instruments.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No instruments found for the category " + categoryId);
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(instruments);
     }
 }
