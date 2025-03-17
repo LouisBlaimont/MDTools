8e1462e0f3f59fbce6eef9364a7c400ed58270fc
Louan Robert <1413-louan.robert@users.noreply.gitlab.uliege.be>
2025-03-17 14:57:42 +0100

    fix authentication

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -2,10 +2,13 @@ import { PUBLIC_API_URL } from "$env/static/public";
 import { PUBLIC_OIDC_ENDPOINT } from "$env/static/public";
 import { user } from "$lib/stores/user_stores"; 
 import { apiFetch } from "$lib/utils/fetch";
+import { createPersistentStore, clearPersistentStore } from "$lib/utils/persistentStore";
 
 
 export function login() {
   window.location.href = PUBLIC_API_URL + "/oauth2/authorization/" + PUBLIC_OIDC_ENDPOINT; // Backend URL for OAuth login
+  user.set(null);
+  clearPersistentStore("user");
 }
 
 // check if user is logged in using the API
@@ -17,4 +20,5 @@ export async function checkUser() {
   } else {
     user.set(null);
   }
+  console.log("User: ", user);
 }
--- frontend/src/lib/stores/user_stores.js
+++ frontend/src/lib/stores/user_stores.js
@@ -2,9 +2,8 @@ import { writable, derived } from "svelte/store";
 import { createPersistentStore } from "../utils/persistentStore";
 
 export const user = createPersistentStore("user", null);
-export const isLoggedIn = writable(false);
+export const isLoggedIn = derived(user, ($user) => $user !== null);
 export const isAdmin = derived(user, ($user) => $user?.roles?.includes("ROLE_ADMIN"));
 export const isUser = derived(user, ($user) => $user?.roles?.includes("ROLE_USER"));
 export const isWebmaster = derived(user, ($user) => $user?.roles?.includes("ROLE_WEBMASTER"));
 // for admin view: export const user = writable({ role: 'admin' });
-
