c4e2f459bddf386461de18c78d62531d2f7251aa
Estelle_Kmln <estelle.keymeulen@student.uliege.be>
2025-03-28 09:33:57 +0100

    Changements of the elements inside instrument modal to correspond to client request

--- frontend/src/lib/modals/editInstrumentModal.svelte
+++ frontend/src/lib/modals/editInstrumentModal.svelte
@@ -117,8 +117,9 @@
   
 {#if isOpen}
   <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
-  
+    <!-- Transparent Background -->
+    <div class="fixed inset-0 bg-gray-500/50 transition-opacity" aria-hidden="true"></div>
+
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
         <div
@@ -146,7 +147,7 @@
                 </div>
                 <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                   <h3 class="text-base font-semibold text-gray-900" id="modal-title">
-                    Modifier un instrument
+                    Modifier l'instrument {reference}
                   </h3>
                   {#await promise}
                     <div role="status" class="my-6 flex items-center justify-center">
@@ -171,15 +172,6 @@
                   {:then}
                     <div class="mt-2">
                       <label class="block my-2 text-sm font-medium text-gray-900" for="user_avatar"
-                        >Référence</label
-                      >
-                      <input
-                        type="text"
-                        bind:value={reference}
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                      />
-
-                      <label class="block my-2 text-sm font-medium text-gray-900" for="user_avatar"
                         >Image</label
                       >
                       <input
@@ -194,19 +186,45 @@
                       {/if}
                     </div>
                     <div class="my-2">
-                      <h3 class="text-base text-gray-900">Characteristics</h3>
+                      <h3 class="text-base text-gray-900">Caractéristiques</h3>
                       <div class="grid grid-cols-2 gap-4">
                         {#each characteristics as characteristic}
-                          {#if characteristic.name !== 'id' && characteristic.name !== 'picturesId' && characteristic.name !== 'categoryId'} 
+                          {#if characteristic.name !== 'id' && characteristic.name !== 'picturesId' && characteristic.name !== 'categoryId' && characteristic.name !== 'alt'} 
                             <div>
                               <label class="text-sm font-medium text-gray-900" for="user_avatar"
                                 >{characteristic.name}</label
                               >
-                              <input
-                                type="text"
-                                bind:value={characteristic.value} onchange={() => (characteristicsEdited = true)}
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                              />
+
+                              <!-- Check if the characteristic is 'obsolete' -->
+                              {#if characteristic.name === 'obsolete'}
+                                <div class="flex items-center gap-4 mt-1">
+                                  <label class="flex items-center">
+                                    <input
+                                      type="radio"
+                                      bind:group={characteristic.value}
+                                      value={true}
+                                      class="mr-2"
+                                    />
+                                    Oui
+                                  </label>
+                                  <label class="flex items-center">
+                                    <input
+                                      type="radio"
+                                      bind:group={characteristic.value}
+                                      value={false}
+                                      class="mr-2"
+                                    />
+                                    Non
+                                  </label>
+                                </div>
+                              {:else}
+                                <input
+                                  type="text"
+                                  bind:value={characteristic.value}
+                                  onchange={() => (characteristicsEdited = true)}
+                                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                />
+                              {/if}
                             </div>
                           {/if}
                         {/each}
